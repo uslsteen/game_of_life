5ff49ff9a73231835bbb3156d9d0c47f19443948
uslsteen <shurygin0171@gmail.com>
2022-10-25 14:27:31 +0300

    [game_life] continue code simpl.
    add bubble swap instead pointer
    delete field from func args

--- main.c
+++ main.c
@@ -14,36 +14,34 @@ typedef struct field_t {
   uint32_t heigth;
 } field_t;
 
+field_t main_field, tmp_field;
+
 enum State { DEAD = 0, ALIVE = 1, NONE };
 
 //
-void field_init(field_t *surf) {
+void field_init() {
   //
-  surf->width = WIDTH;
-  surf->heigth = HEIGHT;
-
-  for (size_t x = 0; x < surf->width; ++x)
-    for (size_t y = 0; y < surf->heigth; ++y) {
+  for (size_t x = 0; x < WIDTH; ++x)
+    for (size_t y = 0; y < HEIGHT; ++y) {
       double radius = (double)rand() / RAND_MAX;
-      surf->field[y][x] = radius < EPS;
+      main_field.field[y][x] = radius < EPS;
     }
 }
 
 //
-int get_cell(field_t *surf, cell_t cell) {
+int get_cell(cell_t cell) {
 
-  int x = (cell.x + surf->width) % surf->width,
-      y = (cell.y + surf->heigth) % surf->heigth;
-  return surf->field[y][x];
+  int x = (cell.x + WIDTH) % WIDTH, y = (cell.y + HEIGHT) % HEIGHT;
+  return main_field.field[y][x];
 }
 
 //
-void set_cell(field_t *surf, cell_t cell, enum State state) {
-  surf->field[cell.y][cell.x] = state;
+void set_cell(cell_t cell, enum State state) {
+  tmp_field.field[cell.y][cell.x] = state;
 }
 
 //
-int get_neighbours_num(field_t *surf, cell_t cell) {
+int get_neighbours_num(cell_t cell) {
   int num = 0;
   for (int id_x = -1; id_x <= 1; ++id_x) {
     for (int id_y = -1; id_y <= 1; ++id_y) {
@@ -51,23 +49,23 @@ int get_neighbours_num(field_t *surf, cell_t cell) {
       if ((id_x == 0) && (id_y == 0))
         continue;
       else
-        num += get_cell(surf, other_cell);
+        num += get_cell(other_cell);
     }
   }
   return num;
 }
 
 //
-void make_next_gen(field_t *surf1, field_t *surf2) {
+void make_next_gen() {
   //
-  for (size_t y = 0; y < surf1->heigth; ++y) {
-    for (size_t x = 0; x < surf1->width; ++x) {
+  for (size_t y = 0; y < HEIGHT; ++y) {
+    for (size_t x = 0; x < WIDTH; ++x) {
       //
       cell_t cell = {x, y};
       enum State state = NONE;
-      int neighbrs_num = get_neighbours_num(surf1, cell);
+      int neighbrs_num = get_neighbours_num(cell);
 
-      if (get_cell(surf1, cell)) {
+      if (get_cell(cell)) {
         if (neighbrs_num == 2 || neighbrs_num == 3)
           state = ALIVE;
         else
@@ -78,27 +76,19 @@ void make_next_gen(field_t *surf1, field_t *surf2) {
         else
           state = DEAD;
       }
-      set_cell(surf2, cell, state);
+      set_cell(cell, state);
     }
   }
 }
 //
 
-void draw_field(field_t *surf) {
+void draw_field() {
   //
-  for (size_t x = 0; x < surf->width; ++x) {
-    for (size_t y = 0; y < surf->heigth; ++y) {
+  for (size_t x = 0; x < WIDTH; ++x) {
+    for (size_t y = 0; y < HEIGHT; ++y) {
       cell_t cell = {x, y};
-
-      int cur_cell = get_cell(surf, cell);
-      /*
-      if (cur_cell == 1)
-        set_pixel(cell.x, cell.y, GREEN);
-      else if (cur_cell == 0)
-        set_pixel(cell.x, cell.y, BLACK);
-      */
-     
-      switch (get_cell(surf, cell)) {
+      //
+      switch (get_cell(cell)) {
       case 1: {
         set_pixel(cell.x, cell.y, GREEN);
         break;
@@ -115,30 +105,34 @@ void draw_field(field_t *surf) {
 }
 
 /**
- * @brief 
- * 
- * @param surf1 
- * @param surf2 
+ * @brief
+ *
+ * @param main_field
+ * @param tmp_field
  */
-void swap(field_t *surf1, field_t *surf2) {
-  field_t tmp = *surf1;
-  *surf1 = *surf2;
-  *surf2 = tmp;
+void swap() {
+  uint8_t tmp = 0;
+  //
+  for (size_t x = 0; x < WIDTH; ++x) {
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        tmp = main_field.field[y][x];
+        main_field.field[y][x] = tmp_field.field[y][x];
+        tmp_field.field[y][x] = tmp;
+    }
+  }
 }
 
-field_t main_field, tmp_field;
-
+//
 int main() {
   graph_init();
   field_init(&main_field);
-  field_init(&tmp_field);
 
   while (is_open_window()) {
     window_clear(BLACK);
     //
-    make_next_gen(&main_field, &tmp_field);
-    draw_field(&main_field);
-    swap(&main_field, &tmp_field);
+    make_next_gen();
+    draw_field();
+    swap();
     //
     flush();
   }
